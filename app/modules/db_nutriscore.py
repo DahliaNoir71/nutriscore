0f3e0559b967eabcc7274846e3e579f1af2cb232
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-22 12:33:45 +0200

    Rename and refactor database module
    Renamed `database.py` to `db_nutriscore.py` for clarity and updated file paths accordingly. Added docstrings to `create_db_from_csv` and `set_db_nutriscore` functions for better code documentation.

--- app/modules/db_nutriscore.py
+++ app/modules/db_nutriscore.py
@@ -3,13 +3,18 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 DB_NAME = "nutriscore.db"
+DB_FULL_PATH = "app/static/" + DB_NAME
 TABLE_NAME = "produits"
-DB_FULL_PATH = "db/" + DB_NAME
 CSV_NAME = "en.openfoodfacts.org.products.csv"
-CSV_FULL_PATH = "csv/" + CSV_NAME
+CSV_FULL_PATH = "app/static/" + CSV_NAME
 CHUNK_SIZE = 10000
 
 def create_db_from_csv():
+    """
+    Creates a SQLite database and populates it with data from a CSV file.
+
+    :return: None
+    """
     # Crée une connexion à la base de données SQLite
     engine = create_engine('sqlite:///' + DB_FULL_PATH)
     # Insertion du CSV par lots
@@ -25,8 +30,13 @@ def create_db_from_csv():
         print(f"Insertion d'un chunk de {len(chunk)} lignes")
         count += 1
 
-def set_database():
+def set_db_nutriscore():
+    """
+    Checks if the database file exists; if not, creates the database from a CSV file.
+
+    :return: None
+    """
     if not os.path.isfile(DB_FULL_PATH):
         create_db_from_csv()
     else:
-        print("Base de données " + DB_NAME + "déjà créée")
+        print("Base de données " + DB_NAME + " déjà créée")
