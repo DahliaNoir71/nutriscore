5709e2e6862f3ef077fc1a862102add12b1eba1a
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-22 12:33:10 +0200

    Initialize Nutri-Score database in Flask app
    Enhanced the Flask app initialization by adding a function to setup Nutri-Score database. This ensures the Nutri-Score database is properly configured alongside the existing database during app startup.

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from config import Config
 from app.modules.database import db
+import app.modules.db_nutriscore as mod_db_nutriscore
 
 def create_app():
+    """
+    Initializes and configures the Flask application.
+
+    :return: The configured Flask application instance.
+    """
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Initialize and bind the database
+    # Initialize and bind the databases
     db.init_app(app)
+    mod_db_nutriscore.set_db_nutriscore()
 
     # Import and register blueprints
     from app.modules.routes import main
