e67771a283b3255a7bade79d8539910dea6fc2c8
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-22 12:35:29 +0200

    Add detailed docstring to Product class
    Enhanced the Product class by adding a descriptive docstring. This docstring outlines class attributes and methods, providing clear documentation for future developers.

--- app/modules/models.py
+++ app/modules/models.py
@@ -3,6 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Product(db.Model):
+    """
+        class Product(db.Model):
+
+        Represents a product in the database.
+
+    Attributes:
+        id (int): Database primary key; unique identifier for the product.
+        name (str): Name of the product; maximum length is 150 characters; cannot be null.
+        energy (float): Energy content of the product; cannot be null.
+        fat (float): Fat content of the product; cannot be null.
+        saturated_fat (float): Saturated fat content of the product; cannot be null.
+        sugar (float): Sugar content of the product; cannot be null.
+        salt (float): Salt content of the product; cannot be null.
+        nutri_score (str): Nutri-score of the product; single character; cannot be null.
+
+    Methods:
+        __repr__: Returns a string representation of the product instance.
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     energy = db.Column(db.Float, nullable=False)
