fe346677d3a3f7b5ac3635494fb4b939fc37d85f
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-23 10:11:13 +0200

    Refactor clean_csv script and update column selections
    Reformat `SELECTED_COLS` and `COLS_STAT` lists for readability and add new columns. Replace `main()` with `clean_csv()` and remove unnecessary whitespace.

--- app/modules/clean_csv.py
+++ app/modules/clean_csv.py
@@ -2,21 +2,46 @@ import pandas as pd
 
 # Constantes
 SELECTED_COLS = [
-    "code", "product_name", "quantity", "brands", "categories",
-    "ingredients_text", "nutriscore_score", "nutriscore_grade",
-    "energy-kj_100g", "energy-kcal_100g", "fat_100g",
-    "saturated-fat_100g", "omega-3-fat_100g", "omega-6-fat_100g",
-    "sugars_100g", "added-sugars_100g", "fiber_100g",
-    "proteins_100g", "salt_100g",
-    "fruits-vegetables-nuts-estimate-from-ingredients_100g"
+    "code",
+    "product_name",
+    "quantity",
+    "brands",
+    "categories",
+    "ingredients_text",
+    "nutriscore_score",
+    "nutriscore_grade",
+    "energy-kj_100g",
+    "energy-kcal_100g",
+    "fat_100g",
+    "saturated-fat_100g",
+    "omega-3-fat_100g",
+    "omega-6-fat_100g",
+    "sugars_100g",
+    "added-sugars_100g",
+    "fiber_100g",
+    "proteins_100g",
+    "salt_100g",
+    "fruits-vegetables-nuts-estimate-from-ingredients_100g",
+    "countries",
+    "countries_tags",
+    "countries_en"
 ]
 
 COLS_STAT = [
-    "nutriscore_score", "nutriscore_grade", "energy-kj_100g",
-    "energy-kcal_100g", "fat_100g", "saturated-fat_100g",
-    "omega-3-fat_100g", "omega-6-fat_100g", "sugars_100g",
-    "added-sugars_100g", "fiber_100g", "proteins_100g",
-    "salt_100g", "fruits-vegetables-nuts-estimate-from-ingredients_100g"
+    "nutriscore_score",
+    "nutriscore_grade",
+    "energy-kj_100g",
+    "energy-kcal_100g",
+    "fat_100g",
+    "saturated-fat_100g",
+    "omega-3-fat_100g",
+    "omega-6-fat_100g",
+    "sugars_100g",
+    "added-sugars_100g",
+    "fiber_100g",
+    "proteins_100g",
+    "salt_100g",
+    "fruits-vegetables-nuts-estimate-from-ingredients_100g"
 ]
 
 DIRECTORY_PATH = "../static/"
@@ -37,7 +62,6 @@ def read_csv_chunks(file_path, selected_columns, chunk_size):
     selected_chunks = [chunk[selected_columns] for chunk in chunks]
     return selected_chunks
 
-
 def filter_and_clean_data(dataframes, selected_columns, cols_stat, nutri_ok):
     print("\nFiltrage et nettoyage des datas")
     list_df_not_na = [
@@ -49,14 +73,10 @@ def filter_and_clean_data(dataframes, selected_columns, cols_stat, nutri_ok):
     df_not_na[cols_stat] = df_not_na[cols_stat].fillna(0)
     return df_not_na
 
-
-def main():
+def clean_csv():
     print("\nDébut de script clean_csv")
     file_path = DIRECTORY_PATH + FILE_NAME
     chunks = read_csv_chunks(file_path, SELECTED_COLS, CHUNK_SIZE)
     clean_data = filter_and_clean_data(chunks, SELECTED_COLS, COLS_STAT, NUTRI_OK)
     clean_data.to_csv(DIRECTORY_PATH + OUTPUT_NAME, sep='\t', index=False)
-
-
-if __name__ == "__main__":
-    main()
+    print("\nFin de script clean_csv")
