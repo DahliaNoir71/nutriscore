59a0c2ab6050bf72d30c176a1372ef58b5edc93d
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-25 15:16:53 +0200

    Add COLS_100G constant to config.py
    This commit introduces a new constant, COLS_100G, to the config.py file. The constant includes a list of nutritional metrics measured per 100g, similar to the existing COLUMNS_100G. This separation improves code organization and readability.

--- config.py
+++ config.py
@@ -94,6 +94,20 @@ class Config:
         "salt_100g",
         "fruits-vegetables-nuts-estimate-from-ingredients_100g"
     ]
+    COLS_100G = [
+        "energy-kj_100g",
+        "energy-kcal_100g",
+        "fat_100g",
+        "saturated-fat_100g",
+        "omega-3-fat_100g",
+        "omega-6-fat_100g",
+        "sugars_100g",
+        "added-sugars_100g",
+        "fiber_100g",
+        "proteins_100g",
+        "salt_100g",
+        "fruits-vegetables-nuts-estimate-from-ingredients_100g"
+    ]
     DIRECTORY_PATH = "../static/"
     OUTPUT_NAME = 'openfoodfact_clean.csv'
     NUTRI_OK = ["a", "b", "c", "d", "e"]
